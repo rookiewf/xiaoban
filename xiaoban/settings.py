0eb0a80022c2b02134d489157b211483aecc7ca0
rookie <rookiewf@aliyun.com>
2020-03-11 14:04:43 +0800

    连接数据库

--- xiaoban/settings.py
+++ xiaoban/settings.py
@@ -68,8 +68,11 @@ WSGI_APPLICATION = 'xiaoban.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'xiaoban',
+        'USER':'rookie',
+        'PASSWORD':123456,
+        'PORT':3306
     }
 }
 
